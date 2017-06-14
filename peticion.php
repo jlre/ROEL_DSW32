7b9fd4f0ac17e7720e1fba8cf9d111bf9794a2d3
jlre <1602085@utrivieramaya.edu.mx>
2017-06-14 10:01:20 -0500

    ROEL_DSW32
    JoseLuis Rodriguez Escobar

---
+++ peticion.php
@@ -0,0 +1,10 @@
+<?php
+<<<<<<< HEAD
+echo $_GET["nom"];
+echo $_GET["edad"];
+=======
+echo '<p id="nombre">'.$_GET['nombre']."</p>";
+echo '<p id="edad">'.$_GET['edad']."</p>";
+>>>>>>> 97cc55f9af456c47da789c16a25d2097e3c71f2c
+
+?>
